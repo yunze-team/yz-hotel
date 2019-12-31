7258c2625a1a81e14afc7a9974a4583ec1c6e9bb
yandong <20012002er@gmail.com>
2019-12-31 18:04:42 +0800

    finish meit goods search method

--- hotel-core/src/main/java/com/yzly/core/service/meit/MeitService.java
+++ hotel-core/src/main/java/com/yzly/core/service/meit/MeitService.java
@@ -5,7 +5,9 @@ import com.alibaba.fastjson.JSONObject;
 import com.yzly.core.domain.HotelSyncList;
 import com.yzly.core.domain.dotw.HotelAdditionalInfo;
 import com.yzly.core.domain.dotw.HotelInfo;
+import com.yzly.core.domain.dotw.RoomBookingInfo;
 import com.yzly.core.domain.dotw.RoomType;
+import com.yzly.core.domain.event.EventAttr;
 import com.yzly.core.domain.meit.MeitCity;
 import com.yzly.core.domain.meit.MeitTraceLog;
 import com.yzly.core.domain.meit.dto.*;
@@ -15,10 +17,13 @@ import com.yzly.core.repository.HotelSyncListRepository;
 import com.yzly.core.repository.dotw.HotelAdditionalInfoRepository;
 import com.yzly.core.repository.dotw.HotelInfoRepository;
 import com.yzly.core.repository.dotw.RoomTypeRepository;
+import com.yzly.core.repository.event.EventAttrRepository;
 import com.yzly.core.repository.meit.MeitCityRepository;
 import com.yzly.core.repository.meit.MeitTraceLogRepository;
 import lombok.extern.apachecommons.CommonsLog;
 import org.apache.commons.lang.StringUtils;
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -26,10 +31,10 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import javax.persistence.criteria.Predicate;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 /**
  * @author lazyb
@@ -52,6 +57,12 @@ public class MeitService {
     private HotelAdditionalInfoRepository hotelAdditionalInfoRepository;
     @Autowired
     private RoomTypeRepository roomTypeRepository;
+    @Autowired
+    private EventAttrRepository eventAttrRepository;
+
+    private static final String MEIT_ROOM_PRICE_RATE = "MEIT_ROOM_PRICE_RATE";
+
+
 
     /**
      * 增加或修改美团调用日志
@@ -216,6 +227,89 @@ public class MeitService {
         img.setImageOrigin(image.getString("url"));
         img.setImageType(imageType);
         return img;
+    }
+
+    /**
+     * 通过roombookinginfo实体封装美团room视图
+     * @param roomBookingInfo
+     * @return
+     */
+    public Room assemblyMeitRoom(RoomBookingInfo roomBookingInfo) {
+        Room room = new Room();
+        Breakfast breakfast = new Breakfast();
+        // 判断房型是否还有早餐
+        if (!roomBookingInfo.getRateBasisId().equals("1331")) {
+            breakfast.setCount(0);
+        } else {
+            breakfast.setCount(2);
+        }
+        JSONObject dates = JSONObject.parseObject(roomBookingInfo.getDates());
+        List<DayInfo> dayInfos = new ArrayList<>();
+        // 获得酒店价格上浮利率
+        EventAttr attr = eventAttrRepository.findByEventType(MEIT_ROOM_PRICE_RATE);
+        Double priceRate = Double.valueOf(attr.getEventValue());
+        if (dates.getString("@count").equals("1")) {
+            JSONObject date = dates.getJSONObject("date");
+            DayInfo dayInfo = generateDayInfoByJson(date, priceRate);
+            dayInfos.add(dayInfo);
+        } else {
+            JSONArray dateArray = dates.getJSONArray("date");
+            for (int i = 0; i < dateArray.size(); i++) {
+                JSONObject date = dateArray.getJSONObject(i);
+                DayInfo dayInfo = generateDayInfoByJson(date, priceRate);
+                dayInfos.add(dayInfo);
+            }
+        }
+        room.setDayInfos(dayInfos);
+        room.setBreakfast(breakfast);
+        room.setRoomId(roomBookingInfo.getRoomTypeCode());
+        room.setRoomName(roomBookingInfo.getName());
+        room.setRatePlanCode(roomBookingInfo.getAllocationDetails());
+        HotelAdditionalInfo hotelAdditionalInfo = hotelAdditionalInfoRepository.findOneByHotelId(roomBookingInfo.getHotelId());
+        room.setCheckInTime(hotelAdditionalInfo.getHotelCheckIn());
+        room.setCheckOutTime(hotelAdditionalInfo.getHotelCheckOut());
+        List<RefundRule> refundRules = new ArrayList<>();
+        // 退订规则组装
+        JSONObject cancellationRules = JSONObject.parseObject(roomBookingInfo.getCancellationRules());
+        if (cancellationRules.getString("@count").equals("1")) {
+            RefundRule refundRule = new RefundRule();
+            refundRule.setReturnable(false);
+            refundRules.add(refundRule);
+        } else {
+            JSONArray cancelArray = cancellationRules.getJSONArray("rule");
+            for (int i = 0; i < cancelArray.size() - 1; i++) {
+                JSONObject rule = cancelArray.getJSONObject(i);
+                RefundRule refundRule = new RefundRule();
+                refundRule.setReturnable(true);
+                refundRule.setRefundType(1);
+                DateTime hotelCheckIn = DateTime.parse(roomBookingInfo.getFromDate() + " " + room.getCheckInTime(), DateTimeFormat.forPattern("yyyy-MM-dd HH:mm"));
+                if (StringUtils.isNotEmpty(rule.getString("fromDate"))) {
+                    DateTime fromDate = DateTime.parse(rule.getString("fromDate"), DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss"));
+                    long maxCheckInTime = (hotelCheckIn.getMillis() - fromDate.getMillis()) % (1000 * 60 * 60 * 24) / (1000 * 60 * 60);
+                    Integer maxCheckIn = maxCheckInTime > 0 ? Integer.valueOf(String.valueOf(maxCheckInTime)) : 0;
+                    refundRule.setMaxHoursBeforeCheckIn(maxCheckIn);
+                }
+                DateTime toDate = DateTime.parse(rule.getString("toDate"), DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss"));
+                long minCheckInTime = (hotelCheckIn.getMillis() - toDate.getMillis()) % (1000 * 60 * 60 * 24) / (1000 * 60 * 60);
+                Integer minCheckIn = minCheckInTime > 0 ? Integer.valueOf(String.valueOf(minCheckInTime)) : 0;
+                refundRule.setMinHoursBeforeCheckIn(minCheckIn);
+                refundRule.setFine(new BigDecimal(rule.getJSONObject("cancelCharge").getString("#text"))
+                        .multiply(new BigDecimal(100)).setScale(0, RoundingMode.HALF_UP).intValue());
+                refundRules.add(refundRule);
+            }
+        }
+        return room;
+    }
+
+    private DayInfo generateDayInfoByJson(JSONObject date, Double priceRate) {
+        DayInfo dayInfo = new DayInfo();
+        BigDecimal basePrice = new BigDecimal(date.getJSONObject("price").getString("#text"));
+        BigDecimal finalPrice = basePrice.multiply(new BigDecimal(100)).multiply(new BigDecimal(1 + priceRate));
+        dayInfo.setRoomPrice(finalPrice.setScale(0, RoundingMode.HALF_UP).intValue());
+        dayInfo.setDate(date.getString("@datetime"));
+        dayInfo.setStatus(1);
+        dayInfo.setCounts(1);
+        return dayInfo;
     }
 
 }
