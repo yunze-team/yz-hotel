34b0234ee968dfd9dfed81c6d6afd6623aa845ee
yandong <20012002er@gmail.com>
2020-01-02 18:59:23 +0800

    add meit order create method

---
+++ hotel-core/src/main/java/com/yzly/core/domain/meit/dto/GuestInfo.java
@@ -0,0 +1,21 @@
+package com.yzly.core.domain.meit.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * meit订单创建参数视图中的客人信息视图
+ * @author lazyb
+ * @create 2020/1/2
+ * @desc
+ **/
+@Setter
+@Getter
+public class GuestInfo {
+
+    private Integer seq;// 客人序号
+    private Integer roomSeq;// 房间序号
+    private String firstName;// 名
+    private String lastName;// 姓
+
+}
---
+++ hotel-core/src/main/java/com/yzly/core/domain/meit/dto/OrderInfo.java
@@ -0,0 +1,20 @@
+package com.yzly.core.domain.meit.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * meit订单创建视图中的订单信息视图
+ * @author lazyb
+ * @create 2020/1/2
+ * @desc
+ **/
+@Getter
+@Setter
+public class OrderInfo {
+
+    private Long orderId;// 美团酒店id
+    private String mobile;// 联系电话
+    private String persionName;// 联系人姓名
+
+}
