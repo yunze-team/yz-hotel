adac93d97d96e53caa6413078e9fcc27ad6cf406
tanyuan <ty139444626@163.com>
2020-06-04 14:15:20 +0800

    测试类

--- hotel-api/src/test/java/com/yzly/api/Test2Xml.java
+++ hotel-api/src/test/java/com/yzly/api/Test2Xml.java
@@ -1,9 +1,7 @@
 package com.yzly.api;
 
-import com.yzly.api.service.jielv.PushAvailabilityAndInventoryService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -11,11 +9,9 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class Test2Xml {
 
-    @Autowired
-    private PushAvailabilityAndInventoryService pushAvailabilityAndInventoryService;
 
     @Test
     public  void test2XML(){
-        pushAvailabilityAndInventoryService.PushAvailabilityAndInventory();
+        System.out.println("试一下");
     }
 }
